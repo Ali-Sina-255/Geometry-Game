ed79db5ce3ad8a34270e80ab05c617eb1fd6afc0
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-11-30 12:00:36 +0430

    rectangle class and method is done

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+
+    def falls_in_rectangle(self, rectangle):
+        if (
+            rectangle.point1.x < self.x < rectangle.point2.x
+            and rectangle.point1.y <= self.y <= rectangle.point2.y
+        ):
+            return True
+        return False
+
+
+class Rectangle:
+    def __init__(self, rectangle) -> None:
+        pass
+
+
+class Rectangle:
+    def __init__(self, point1: float, point2: float) -> None:
+        self.point1 = point1
+        self.point2 = point2
