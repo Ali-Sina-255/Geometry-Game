5a98386b7a839e5818f308c3009cb6f5856faf5a
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-11-30 12:11:23 +0430

    area mehtod and point rectangle object is created

--- main.py
+++ main.py
@@ -13,11 +13,34 @@ class Point:
 
 
 class Rectangle:
-    def __init__(self, rectangle) -> None:
-        pass
-
-
-class Rectangle:
     def __init__(self, point1: float, point2: float) -> None:
         self.point1 = point1
         self.point2 = point2
+
+    def area(self):
+        return (self.point2.x - self.point1.x) * (self.point2.y - self.point1.y)
+
+
+rectangle = Rectangle(
+    Point(randint(0, 9), randint(0, 9)), Point(randint(10, 19), randint(10, 19))
+)
+
+# Print rectangle coordinates
+print(
+    "Rectangle Coordinates: ",
+    rectangle.point1.x,
+    ",",
+    rectangle.point1.y,
+    "and",
+    rectangle.point2.x,
+    ",",
+    rectangle.point2.y,
+)
+
+# Get point and area from user
+user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
+user_area = float(input("Guess rectangle area: "))
+
+# Print out the game result
+print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
+print("Your area was off by: ", rectangle.area() - user_area)
